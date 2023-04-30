8ff8980eca40aa445f2a777c95d7c214853598fe
shinjisogabe <124783728+sogabeshinji@users.noreply.github.com>
2023-04-30 00:48:24 +0000

    [edit]routes,views

--- nagano_cake/config/routes.rb
+++ nagano_cake/config/routes.rb
@@ -4,11 +4,33 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   namespace :admin do
-    resources :items
+    root to: 'homes#top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :updete] do
+    end
+    resources :customers, only: [:index, :show, :edit, :updete] do
+    end
+    resources :orders, only: [:show] do
+    end
   end
   
   namespace :public do
-    resources :items
+    root to: 'homes#top'
+    get '/home/about' => 'homes#about', as: 'about'
+    resources :items, only: [:index, :show] do
+    end
+    resources :registration, only: [:new, :create] do
+    end
+    resources :session, only: [:new, :create, :destroy] do
+    end
+    resources :customers, only: [:show, :edit, :updete] do
+    end
+      #add get :out, patch :withdraw
+    resources :cart_items, only: [:index, :update, :destroy, :create] do
+    end
+      # add delete :destroy_all
+    resources :orders, only: [:new, :create, :index, :show] do
+      # add post :confirm, get :complete,
+    end
   end
-  
+
 end
